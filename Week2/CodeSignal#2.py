0bdbf707b61ecc034601816927b55df8a9cca980
mousenpenguin <55570739+mousenpenguin@users.noreply.github.com>
2019-10-17 14:16:10 +0800

    Update CodeSignal#2.py

--- Week2/CodeSignal#2.py
+++ Week2/CodeSignal#2.py
@@ -24,22 +24,7 @@ def shapeArea(n):
     for i in range(1, len(sequence)):
         value = sequence[i]
         
-        # if value > previous one, then it goes well so far (no need to do anything else).
         if (value > sequence[i - 1]):
             continue
         
-        # otherwise there is an error, and the errors count increments by 1
-        errors += 1
 
-        # if there are two errors then there is no solution.
-        if (errors > 1):
-            return False
-        
-        # but if there is only one error, then we check that the index is in the sequence array range
-        if (i - 2 >= 0 and i + 1 < len(sequence)):
-            # and check whether we can make the sequence consecutive
-            # if we can't, then there is no solution due there is an unsolvable error.
-            if (value <= sequence[i - 2] and sequence[i + 1] <= sequence[i - 1]):
-                return False
-        
-    return True
